df581b599886d57c5d41d645baca917f79ff5e60
Hudrel <c.steeve@orange.fr>
2024-01-14 23:22:47 +0100

    0.0.1
    Débuts du navigateur NavIgnite.
    
    - Création du projet
    - Recherches de la technologie à choisir → WebView2
    - Création d’une barre pour saisir l’URL, ajout du bouton et de la fonctionnalité de recherche
    - Message d’alerte lorsque l’on tente d’accéder à un site en HTTP.
    - Mise à jours automatique de l’URL du site via requête au serveur

--- NavIgnite/MainWindow.xaml.cs
+++ NavIgnite/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Web.WebView2.Core;
 
 namespace NavIgnite
 {
@@ -23,6 +13,78 @@ namespace NavIgnite
         public MainWindow()
         {
             InitializeComponent();
+            InitializeAsync();
+            webView.NavigationStarting += EnsureHttps;
+        }
+
+        private void AddressBar_KeyDown(object sender, KeyEventArgs e)
+        {
+            //If Enter press --> Go to the URL
+            if (e.Key == Key.Enter)
+            {
+                NavigateToAddress();
+            }
+        }
+
+        private void ButtonGo_Click(object sender, RoutedEventArgs e)
+        {
+            //If ButtonGo press --> Go to the URL
+            NavigateToAddress();
+        }
+
+        private void NavigateToAddress()
+        {
+            //Get the writen URL and go to it
+            if (webView != null && webView.CoreWebView2 != null)
+            {
+                webView.CoreWebView2.Navigate(addressBar.Text);
+            }
+        }
+
+        void EnsureHttps(object sender, CoreWebView2NavigationStartingEventArgs args)
+        {
+            //Check if the URL contain http, if yes show a toolbox to say the website isn't safe
+            String uri = args.Uri;
+            if (!uri.StartsWith("https://"))
+            {
+                webView.CoreWebView2.ExecuteScriptAsync($"alert('{uri} is not safe, try an https link')");
+                args.Cancel = true;
+            }
+        }
+
+        async void InitializeAsync()
+        {
+            await webView.EnsureCoreWebView2Async(null);
+
+            //Get the default URL
+            string defaultUrl = webView.Source?.ToString();
+
+            //Update the URL
+            Dispatcher.Invoke(() =>
+            {
+                addressBar.Text = defaultUrl;
+            });
+
+            webView.CoreWebView2.NavigationCompleted += (s, e) =>
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    addressBar.Text = webView.Source.ToString();
+                });
+            };
+
+            webView.CoreWebView2.WebMessageReceived += UpdateAddressBar;
+        }
+
+        void UpdateAddressBar(object sender, CoreWebView2WebMessageReceivedEventArgs args)
+        {
+            //Update the URL with the new get from server
+            String uri = args.TryGetWebMessageAsString();
+            Dispatcher.Invoke(() =>
+            {
+                addressBar.Text = uri;
+            });
+            webView.CoreWebView2.PostWebMessageAsString(uri);
         }
     }
 }
